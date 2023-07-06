f5c01a1716e896ab06d65de37813e6d42d133f8c
BrunoApolaya30 <n00210272@upn.pe>
2023-07-05 20:31:28 -0500

    mapa

--- chacomboApp/app/src/main/java/com/chacombo/chacomboapp/Ubicanos_mapa.java
+++ chacomboApp/app/src/main/java/com/chacombo/chacomboapp/Ubicanos_mapa.java
@@ -1,14 +1,39 @@
 package com.chacombo.chacomboapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
-public class Ubicanos_mapa extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+public class Ubicanos_mapa extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap mMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ubicanos_mapa);
+        SupportMapFragment mapFragment =
+                (SupportMapFragment)getSupportFragmentManager()
+                        .findFragmentById(R.id.mapa);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        LatLng restaurante = new LatLng(-11.965147, -76.995838);
+        mMap.addMarker(new MarkerOptions()
+                .position(restaurante).title("RESTAURANTE - CHACOMBO CHICHARRONES"));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(restaurante,20));
     }
 }
