bfc3be2584f82e9c86abb540187ae1b3cc493c89
alexander watson <alexander_watsonchaves@hotmail.com>
2023-07-05 20:06:16 -0500

    nueva layout mapa

---
+++ chacomboApp/app/src/main/java/com/chacombo/chacomboapp/Ubicanos_mapa.java
@@ -0,0 +1,14 @@
+package com.chacombo.chacomboapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ubicanos_mapa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ubicanos_mapa);
+    }
+}
